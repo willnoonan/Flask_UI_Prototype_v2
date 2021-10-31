267a8d647da65f3208ced6ca393710f0a9372461
williamnoonan <willnoonan@outlook.com>
2021-10-31 14:02:24 -0500

    In working state. Cut link out of configurations.html and replaced with config variable passed in through configurations_view function. Had to add __str__ method to Configuration model so config variable in html prints the name of the model.

--- flaskr/configurations/models.py
+++ flaskr/configurations/models.py
@@ -13,3 +13,6 @@ class Configuration(me.Document):
     # an id field is created automatically, according to documentation
     time_created = me.DateTimeField(default=datetime.now)
     name = me.StringField(required=True)
+
+    def __str__(self):
+        return self.name
