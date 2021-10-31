89e3644a6dc5c0c04580f80ffd6984bcd71e670e
williamnoonan <willnoonan@outlook.com>
2021-10-31 13:47:56 -0500

    Added Configuration model to configurations/models.py using mongoengine Document.

--- flaskr/configurations/models.py
+++ flaskr/configurations/models.py
@@ -1,5 +1,6 @@
 from flaskr.util import GenPage
-
+import mongoengine as me
+from datetime import datetime
 
 class Config(GenPage):
     global_id = 0
@@ -7,3 +8,8 @@ class Config(GenPage):
         super().__init__(name)
         Config.global_id += 1
         self.id = Config.global_id
+
+class Configuration(me.Document):
+    # an id field is created automatically, according to documentation
+    time_created = me.DateTimeField(default=datetime.now)
+    name = me.StringField(required=True)
