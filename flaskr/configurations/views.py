8ab1922ff9d212975dac069357f94b97b00add01
williamnoonan <willnoonan@outlook.com>
2021-10-31 14:26:51 -0500

    Runs as intended; data comes from mongodb instead of user_configs list. Removed all usages of user_configs list. Link to individual config pages restored in configurations.html. Had to change converter type from int to string for single_config_view route in views.py to use objects from mongodb.

--- flaskr/configurations/views.py
+++ flaskr/configurations/views.py
@@ -2,28 +2,23 @@ from flask import Blueprint, redirect, render_template, request, url_for
 from .models import Config
 from .forms import ConfigurationForm
 from .models import Configuration
+from bson.objectid import ObjectId
 
 configurations = Blueprint("configurations", __name__,
                            template_folder="templates")
 
-user_configs = []
 
-def get_config_byid(int_id):
-    config_dict = {config.id:config for config in user_configs}
-    return config_dict.get(int_id)
-
-# The following is the link I had to cut out of configurations.html to test use of Configuration model:
-# <a href="{{ url_for('configurations.single_config_view', config_id=config.id) }}">{{ config }}</a>
 @configurations.route("/configurations")
 def configurations_view():
     configs = Configuration.objects()
     return render_template("configurations/configurations.html", configs=configs)
 
 
-@configurations.route("/configuration/<int:config_id>/")
+@configurations.route("/configuration/<string:config_id>/")
 def single_config_view(config_id):
-    user_config = get_config_byid(config_id)
-    return render_template("configurations/single_config.html", config=user_config)
+    config = Configuration.objects.get(id=config_id)
+    return render_template("configurations/single_config.html", config=config)
+
 
 @configurations.route("/newconfiguration", methods=["GET", "POST"])
 def new_configuration_view():
@@ -32,6 +27,5 @@ def new_configuration_view():
         config = Configuration()
         config.name = form.name.data
         config.save()
-        # user_configs.append(Config(form.name.data))
         return redirect(url_for('.configurations_view'))
     return render_template("configurations/new_config.html", form=form)
