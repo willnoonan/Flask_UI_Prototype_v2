b10bd6a64a009c2764c33ca9deb40a84e2f77a5f
williamnoonan <willnoonan@outlook.com>
2021-11-08 17:24:40 -0600

    added imports to and created a Project class, a mongoengine Document, in projects/models.py

--- flaskr/projects/models.py
+++ flaskr/projects/models.py
@@ -1,5 +1,6 @@
 from flaskr.util import GenPage
-
+import mongoengine as me
+from datetime import datetime
 
 class Proj(GenPage):
     global_id = 0
@@ -7,3 +8,12 @@ class Proj(GenPage):
         super().__init__(name)
         Proj.global_id += 1
         self.id = Proj.global_id
+
+
+class Project(me.Document):
+    # an id field is created automatically, according to documentation
+    time_created = me.DateTimeField(default=datetime.now)
+    name = me.StringField(required=True)
+
+    def __str__(self):
+        return self.name
